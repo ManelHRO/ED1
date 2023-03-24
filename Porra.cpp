feed50a02c44e4a6e1a86fd7b8e39883138e7cd3
ManelHRO <93168394+ManelHRO@users.noreply.github.com>
2023-03-24 15:50:20 -0300

    Update Porra.cpp

--- Porra.cpp
+++ Porra.cpp
@@ -1,45 +1,43 @@
-#include <bits/stdc++.h>
-#define N 51
+#include<bits/stdc++.h>
 using namespace std;
-char mt[N][N];
-bool vis[N][N];
-int dx[]={-1,0,1,0,-1,1,1,-1};
-int dy[]={0,1,0,-1,1,1,-1,-1};
-int x,y;
+#define M 101
+#define N 101
+char mt[M][N];
+bool vis[M][N];
+int dx[]={-1,0,1,0,-1,1,1,-1},dy[]={0,1,0,-1,1,1,-1,-1},x,y;
 
-void war(int n,int i,int j){
-	vis[i][j] = true;
-    for(int k=0;k<8;k++){
-        x=i+dx[k];
-        y=j+dy[k];
-        if(x>=0&&y<n&&y>=0&&x<n&&mt[x][y]=='1'&&!vis[x][y]){
-            return war(n,x,y);
-        }
-    }
+void oil(int m,int n,int i,int j){
+	vis[i][j]=true;
+	
+	for(int k=0;k<8;k++){
+		x=i+dx[k];
+		y=j+dy[k];
+		if(x>=0&&x<m&&y>=0&&y<n&&mt[x][y]=='@'&&!vis[x][y]) oil(m,n,x,y);
+	}
 }
 
 int main(){
-    
-    int n,cont,k=1;
-    
-    while(cin>>n){
-			cont=-1;
-		for(int i=0;i<n;i++){
+	
+	int m,n,cont;
+	
+	while(cin>>m>>n&&m!=0){
+		cont=0;
+		
+		for(int i=0;i<m;i++){
 			for(int j=0;j<n;j++){
 				cin>>mt[i][j];
 			}
 		}
-		for(int i=0;i<n;i++){
+		
+		for(int i=0;i<m;i++){
 			for(int j=0;j<n;j++){
-				if(mt[i][j]=='1'&&!vis[i][j]){
-					cont++;
-					war(n,i,j);
-				}
+				if(mt[i][j]=='@'&&!vis[i][j]) cont++,oil(m,n,i,j);
 			}
 		}
-		cout<<"Image number "<<k++<<" contains "<<cont<<" war eagles."<<endl;
-		for(int i=0;i<n;i++)
-			for(int j=0;j<n;j++) 
-				mt[i][j]=false;
+		cout<<cont<<endl;
+		
+		for(int i=0;i<m;i++)
+			for(int j=0;j<n;j++)
+				vis[i][j]=false;
 	}
 }
